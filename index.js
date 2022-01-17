b3fcd47d677c36fa529cc84898a8288b7626d5c3
Maslenat <91131396+Maslenat@users.noreply.github.com>
2022-01-17 17:21:01 +1000

    add files

---
+++ index.js
@@ -0,0 +1,34 @@
+function calcSum(){
+    let a = prompt('Введите первый аргумент', );
+    let b = prompt('Введите второй аргумент', );
+    alert (Number(a)+Number(b));
+
+   
+}
+function calcSub(){
+    let a = prompt('Введите первый аргумент', );
+    let b = prompt('Введите второй аргумент', );
+    alert (Number(a)-Number(b));
+
+   
+}
+function calcMult(){
+    let a = prompt('Введите первый аргумент', );
+    let b = prompt('Введите второй аргумент', );
+    alert (Number(a)*Number(b));
+
+   
+}
+function calcDiv(){
+    let a = prompt('Введите первый аргумент', );
+    let b = prompt('Введите второй аргумент', );
+    alert (Number(a)/Number(b));
+
+   
+}
+let showMessage=() =>{let name = prompt('Как тебя зовут?', );
+alert(`Привет, ${name}!`); 
+
+
+};
+
