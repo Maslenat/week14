879495ecc78b9bbce8c2e7abfa4d1b2a7f5af990
Maslenat <91131396+Maslenat@users.noreply.github.com>
2022-01-18 15:44:24 +1000

    calculator

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
-function calcSum(){
-    let a = prompt('Введите первый аргумент', );
-    let b = prompt('Введите второй аргумент', );
-    alert (Number(a)+Number(b));
-
-   
-}
-function calcSub(){
-    let a = prompt('Введите первый аргумент', );
-    let b = prompt('Введите второй аргумент', );
-    alert (Number(a)-Number(b));
-
-   
-}
-function calcMult(){
-    let a = prompt('Введите первый аргумент', );
-    let b = prompt('Введите второй аргумент', );
-    alert (Number(a)*Number(b));
-
-   
-}
-function calcDiv(){
-    let a = prompt('Введите первый аргумент', );
-    let b = prompt('Введите второй аргумент', );
-    alert (Number(a)/Number(b));
-
-   
-}
-let showMessage=() =>{let name = prompt('Как тебя зовут?', );
-alert(`Привет, ${name}!`); 
+let showMessage = () => {
+    let name = prompt('Как тебя зовут?', );
+    alert(`Привет, ${name}!`);
 
 
 };
 
+
+function anssum() {
+    var a = document.getElementById('argument1').value;
+    console.log(a);
+    var b = document.getElementById('argument2').value;
+    console.log(b);
+
+    let anssumma = Number(a) + Number(b);
+    console.log(anssumma);
+    let summa = document.getElementById('summa').value = anssumma;
+
+}
+
+function anssub() {
+    var a = document.getElementById('argument1').value;
+    console.log(a);
+    var b = document.getElementById('argument2').value;
+    console.log(b);
+
+    let anssub = Number(a) - Number(b);
+    console.log(anssub);
+    let subb = document.getElementById('sub').value = anssub;
+}
+
+function ansdiv() {
+    var a = document.getElementById('argument1').value;
+    console.log(a);
+    var b = document.getElementById('argument2').value;
+    console.log(b);
+
+    let ansdiv = Number(a) / Number(b);
+    console.log(ansdiv);
+    let div = document.getElementById('div').value = ansdiv;
+}
+
+function ansmult() {
+    var a = document.getElementById('argument1').value;
+    console.log(a);
+    var b = document.getElementById('argument2').value;
+    console.log(b);
+
+    let ansmult = Number(a) * Number(b);
+    console.log(ansmult);
+    let mult = document.getElementById('mult').value = ansmult;
+
+}
